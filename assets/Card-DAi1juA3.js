5f6d924e58d4cec3e7855bbf9d353d70206ff93e
hiowcaheh <hiowcaheh@users.noreply.github.com>
2026-07-16 09:40:53 +0000

    deploy: 4c6deabccfc4b7280df7b7a8008cb8eda046554f

--- assets/Card-DAi1juA3.js
+++
@@ -1,1 +0,0 @@
-import{T as e}from"./queryKeys-BvFRFyux.js";import{at as t}from"./index-BLO7Bu6Q.js";var n=e();function r({interactive:e,className:r,...i}){return(0,n.jsx)(`div`,{className:t(`rounded-(--radius-card) bg-white shadow-(--shadow-card)`,e&&`press cursor-pointer`,r),...i})}export{r as t};
