60b7a84402d0e98be841a9158295bd29fb12dd92
hiowcaheh <hiowcaheh@users.noreply.github.com>
2026-07-17 07:24:03 +0000

    deploy: 8d131b179b7d3b0a2d857cc635bf0cced8284866

---
+++ assets/Card-CPpHu-Gn.js
@@ -0,0 +1,1 @@
+import{J as e}from"./queryKeys-FGR1ZiL-.js";import{Y as t}from"./index-BmvoAtbU.js";var n=e();function r({interactive:e,className:r,...i}){return(0,n.jsx)(`div`,{className:t(`rounded-(--radius-card) bg-white shadow-(--shadow-card)`,e&&`press cursor-pointer`,r),...i})}export{r as t};
